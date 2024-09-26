f2dfa9cfcc39f018c04af2d0537a21ee75c640d2
Kévin Bardoula <kevin@MacBook-Pro-de-Kevin.local>
2024-09-26 16:09:08 +0200

    Code final

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(){
 	/* Variable utiliser dans le code */
 	int number;
 	int i = 0;
+	int recommencer;
 
 	/* Le code ci dessous permet de transmettre un nombre entier aléatoire entre 1 et 100 */
 	srand(time(NULL));
@@ -18,34 +19,61 @@ int main(){
 
 	do{
 		/* Début du jeu*/
+		if(number > 100){
+			printf("Le numéros saisi n'est pas valide car supérieur à 100 !\n");
+		} else if (number < 1) {
+			printf("Le numéros saisi n'est pas valide car inférieur à 1 !\n");
+		}
 		/* Si le numétos et différent le jeu continue */
 		if(number != random_number){
+			if (number < random_number){
+				printf("C'est plus !\n");
+			} else if (number > random_number){
+				printf("C'est moins !\n");
+			}
 			printf("Raté ! Veuillez réessayer !\n");
 			scanf("%d", &number);
 			i++;
 		} 
 		/* Le code si dessous donne un nombre d'essai maximum étant de 10 puisque i commence à 0 */
-		/* Si le joueur ne trouve pas le bon numéros alors la partie prend fin */
+		/* Si le joueur ne trouve pas le bon numéros alors vous avez le choix entre continuer et recommencer */
 		if (i == 9){
-			printf("Dommage, vous avez perdu ! Le numéros à deviner était %d", random_number);
-			exit(0);
+			printf("Dommage, vous avez perdu ! Le numéros à deviner était %d\n", random_number);
+			printf("Voulez vous recommencer ? Tapez 1 si oui sinon taper 0.\n");
+			scanf("%d", &recommencer);
+
+			if(recommencer == 1){
+				i = -1;
+			} else if (recommencer == 0){
+				exit(0);
+			}
 		}
 		/* Le code ci dessous permet de féliciter le joueur ci il trouve le bon numéros */
 		if (number == random_number){
-			printf("Bravo ! Vous avez trouver mon numéros ! Le numéros était de %d\n", random_number);
+			printf("Bravo ! Vous avez trouver mon numéros ! Le numéros était %d\n", random_number);
 		} 
 	}while(number != random_number);
 
 	exit(0);
 }
 
-/* Code bonus
+/* Code bonus */
+/*
 int i = 0;
+int recommencer;
 
 i++;
 
 if (i == 9){
-printf("Dommage, vous avez perdu ! Le numéros à deviner était %d", random_number);
-exit(0);
+	printf("Dommage, vous avez perdu ! Le numéros à deviner était %d", random_number);
+	printf("Voulez vous recommencer ? Tapez 1 si oui sinon taper 0.\n");
+	scanf("%d", recommencer);
+
+	if(recommencer == 1){
+		i = -1;
+	} else if (recommencer == 0){
+		exit(0);
+	}
+
 }
 */
